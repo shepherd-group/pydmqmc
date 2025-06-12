dd4feb6a754bd62e6d5d2c93b3a36d80080b7aca
Claire Kopenhafer <kopenhaf@msu.edu>
2025-06-12 17:18:18 -0400

    Add rk4

--- pydmqmc/utils/integrators.py
+++ pydmqmc/utils/integrators.py
@@ -12,4 +12,16 @@ def euler(func, y, dt, *args, **kwargs):
     return y + dt * dx_dt
 
 def rk4(func, y, dt, *args, **kwargs):
-    pass
+    """
+    Update `y` using the fourth-order Runge-Kutta method.
+
+    Function `func` is of the form::
+
+        dy/dt = func(y, ...)
+    """
+    k1 = func(y, *args, **kwargs)
+    k2 = func(y + dt/2 * k1, *args, **kwargs)
+    k3 = func(y + dt/2 * k2, *args, **kwargs)
+    k4 = func(y + dt * k3, *args, **kwargs)
+
+    return y + dt/6 * (k1 + 2*k2 + 2*k3 + k4)
--- tests/utils/test_integrators.py
+++ tests/utils/test_integrators.py
@@ -6,4 +6,20 @@ from pydmqmc.utils.integrators import *
 
 def test_euler():
     y_prime = euler(np.exp, 1, 0.01)
-    assert np.isclose(y_prime, 1 + 0.01*np.exp(1))
+
+    answer = 1 + 0.01*np.exp(1)
+    assert np.isclose(y_prime, answer)
+
+
+def test_rk4():
+    y_prime = rk4(np.exp, 1, 0.01)
+
+    y = 1
+    h = 0.01
+    k1 = np.exp(y)
+    k2 = np.exp(y + h/2*k1)
+    k3 = np.exp(y + h/2*k2)
+    k4 = np.exp(y + h * k3)
+    answer = y + h/6 * (k1 + 2*k2 + 2*k3 + k4)
+
+    assert np.isclose(y_prime, answer)
