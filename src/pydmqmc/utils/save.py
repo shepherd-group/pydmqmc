cf28369a6522b4d903c2e6b3207b575777873430
Claire Kopenhafer <kopenhaf@msu.edu>
2026-05-08 13:45:21 -0400

    [fix] Better input checking on save_report

--- src/pydmqmc/utils/save.py
+++ src/pydmqmc/utils/save.py
@@ -4,6 +4,8 @@ import csv
 import pickle
 import numpy as np
 
+import warnings
+
 from numpy.typing import ArrayLike
 
 
@@ -77,8 +79,10 @@ def save_report(
     basename : str
         Base filename (i.e., without extension) to which to write data.
     index_col : string, optional
-        Name of the column to put first. Must be a key in the
-        dictionaries in `list_of_dicts`.
+        For "csv" or "txt" filetypes, name of the column to put first.
+        Must be a key in the dictionaries in `list_of_dicts`.
+        If `index_col` is not supplied, the first column saved to file
+        will be arbitrary.
     filetype : str, default "csv"
         File type (aka extension) with which to save `list_of_dicts`.
         Supported types are:
@@ -94,19 +98,18 @@ def save_report(
     --------
     save_array : Save an array as a chosen file type.
     """
-    if list_of_dicts is None:
-        raise RuntimeError(
-            "Provided data is None! Did you remember to run the simulation?"
-        )
+    if not list_of_dicts:
+        raise RuntimeError("No data provided! Did you remember to run the simulation?")
 
     if filetype == "txt" or filetype == "csv":
         filename = basename + "." + filetype
 
-        # Create a list of field names from the dictionaries in
-        # list_of_dicts but with index_col first
+        # Create a list of field names from the dictionaries
         fields = list(list_of_dicts[0].keys())
-        fields.remove(index_col)
-        fields = [index_col] + fields
+        if index_col is not None:
+            # Put index_col first in the list
+            fields.remove(index_col)
+            fields = [index_col] + fields
 
         if filetype == "csv":
             dialect = "excel"  # default dialect
@@ -120,6 +123,8 @@ def save_report(
             writer.writerows(list_of_dicts)
 
     elif filetype == "pkl":
+        if index_col is not None:
+            warnings.warn("Parameter index_col is ignored with 'pkl' filetype.")
         filename = basename + ".pkl"
         with open(filename, "wb") as f:
             pickle.dump(list_of_dicts, f, protocol=pickle_protocol)
