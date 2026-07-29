20081e7c7a57d8397a10070a25bd7f2c972a8836
Claire Kopenhafer <kopenhaf@msu.edu>
2026-07-29 17:06:39 -0400

    [tests] Add tests for new reportable values & for negative valued matrices

--- tests/report/test_report_functions.py
+++ tests/report/test_report_functions.py
@@ -1,4 +1,4 @@
-from pytest import fixture
+from pytest import fixture, raises
 import numpy as np
 
 from pydmqmc.report.report_functions import *
@@ -27,4 +27,34 @@ class TestReportFunctions():
 
     def test_von_neumann_expectation(self):
         assert np.isclose(von_neumann_expectation(self._matrix), -12.5337028)
+
+class TestReportFunctionsNegativeMatrix():
+
+    @fixture(autouse=True)
+    def _setup(self):
+        self._matrix = np.arange(-10,15).reshape((5,5))
+        self._hamiltonian = np.flip(self._matrix)
+
+    def test_total_particles(self):
+        assert np.isclose(total_particles(self._matrix), 160)
+
+    def test_trace(self):
+        assert np.isclose(trace(self._matrix), 10)
+
+    def test_energy_numerator(self):
+        assert np.isclose(energy_numerator(self._matrix, self._hamiltonian),
+                          -400)
+    
+
+    def test_energy_expectation(self):
+        assert np.isclose(energy_expectation(self._matrix, self._hamiltonian),
+                          -40)
+
+    def test_von_neumann_numerator(self):
+        with raises(FloatingPointError):
+            von_neumann_numerator(self._matrix)
+
+    def test_von_neumann_expectation(self):
+        with raises(FloatingPointError):
+            von_neumann_expectation(self._matrix)
 
